1da037753dcacc5d4b40a2e3d182fd473a9d259a
Filip Sufitchi <fsufitchi@gmail.com>
2016-11-23 14:42:03 -0500

    Complete basic upload functionality

--- api/transfernode.go
+++ api/transfernode.go
@@ -2,6 +2,8 @@ package api
 
 import (
 	"errors"
+	"fmt"
+	"io"
 	"mime"
 	"net/http"
 	"os"
@@ -16,9 +18,11 @@ import (
 // TransferNodeConnection holds details about a connection to a transfer node
 type TransferNodeConnection struct {
 	Conn                   *websocket.Conn
+	Closed                 bool
 	AuthKey                string
 	FileID                 string
 	ReceivedFileIDCallback func(string)
+	ProgressCallback       func(uint64)
 	Error                  error
 }
 
@@ -32,34 +36,37 @@ func ConnectTransferNode(url string, auth string) (*TransferNodeConnection, chan
 
 func (conn *TransferNodeConnection) readLoop(doneChan chan error) {
 	defer conn.Conn.Close()
-	for {
-		buf, closed, err := conn.readMessage()
+	conn.Conn.SetCloseHandler(func(code int, text string) error {
+		conn.Closed = true
+		if code != websocket.CloseNormalClosure && code != websocket.CloseNoStatusReceived {
+			return fmt.Errorf("Abnormal closure %d %s", code, text)
+		}
+		return nil
+	})
+	for !conn.Closed {
+		buf, err := conn.readMessage()
+		if conn.Closed {
+			continue
+		}
 		if err != nil {
 			doneChan <- err
-			break
-		}
-
-		if closed {
-			doneChan <- nil
-			break
+			return
 		}
 
 		err = conn.triageMessage(buf)
 		if err != nil {
 			doneChan <- err
-			break
+			return
 		}
 	}
+	doneChan <- nil
 }
 
-func (conn *TransferNodeConnection) readMessage() (buf []byte, closed bool, err error) {
-	closed = false
+func (conn *TransferNodeConnection) readMessage() (buf []byte, err error) {
 	msgType, buf, err := conn.Conn.ReadMessage()
+
 	if err != nil {
 		return
-	}
-	if msgType == websocket.CloseMessage {
-		closed = true
 	} else if msgType != websocket.BinaryMessage {
 		err = errors.New("Unexpected non-binary message type")
 	}
@@ -84,6 +91,12 @@ func (conn *TransferNodeConnection) triageMessage(buf []byte) (err error) {
 		conn.FileID = message.TransferCreatedData.GetTransferId()
 		if conn.ReceivedFileIDCallback != nil {
 			conn.ReceivedFileIDCallback(conn.FileID)
+		}
+		break
+	case protobufs.TransferNodeToClientMessage_PROGRESS:
+		bytesProgress := message.ProgressData.GetBytesUploaded()
+		if conn.ProgressCallback != nil {
+			conn.ProgressCallback(uint64(bytesProgress))
 		}
 		break
 	default:
@@ -129,4 +142,50 @@ func (conn *TransferNodeConnection) SelectFile(path string) {
 
 	data, _ := proto.Marshal(message)
 	conn.Error = conn.Conn.WriteMessage(websocket.BinaryMessage, data)
+}
+
+func (conn *TransferNodeConnection) sendChunk(order int, size uint64, chunk []byte) {
+	message := &protobufs.ClientToTransferNodeMessage{
+		Type: protobufs.ClientToTransferNodeMessage_UPLOAD_DATA,
+		UploadData: &protobufs.UploadData{
+			Order: 0,
+			Size:  size,
+			Data:  chunk[:],
+		},
+		Timestamp: time.Now().Unix(),
+	}
+	data, _ := proto.Marshal(message)
+	conn.Error = conn.Conn.WriteMessage(websocket.BinaryMessage, data)
+}
+
+func (conn *TransferNodeConnection) sendComplete() {
+	message := &protobufs.ClientToTransferNodeMessage{
+		Type:         protobufs.ClientToTransferNodeMessage_FINISHED,
+		FinishedData: &protobufs.FinishedData{},
+		Timestamp:    time.Now().Unix(),
+	}
+	data, _ := proto.Marshal(message)
+	conn.Error = conn.Conn.WriteMessage(websocket.BinaryMessage, data)
+}
+
+func (conn *TransferNodeConnection) UploadFile(path string) error {
+	file, _ := os.Open(path)
+	buf := make([]byte, 50000) // read 50 KB chunks
+	var count int
+	var err error
+	for i := 0; err == nil; i++ {
+		count, err = file.Read(buf)
+		if err != nil && err != io.EOF {
+			fmt.Printf("got err %s", err)
+			return err
+		}
+
+		conn.sendChunk(i, uint64(count), buf[:count])
+		if conn.Error != nil {
+			return conn.Error
+		}
+
+	}
+	conn.sendComplete()
+	return conn.Error
 }
--- filebounce/cli.go
+++ filebounce/cli.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"errors"
 	"flag"
 	"fmt"
 	"os"
+	"time"
 
+	humanize "github.com/dustin/go-humanize"
 	"github.com/fsufitch/filebounce-cli/api"
 )
 
@@ -24,10 +27,28 @@ func getPath() (string, error) {
 	return path, nil
 }
 
+func waitToUpload(delay int, cb func()) {
+	if delay < 1 {
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Println("Hit Enter to start upload. ")
+		_, _ = reader.ReadString('\n')
+	} else {
+		time.Sleep(time.Duration(delay) * time.Second)
+	}
+	cb()
+}
+
 func main() {
 	auth := flag.String("auth", "XXX", "authentication key to send to transfer node")
 	host := flag.String("host", "localhost:8888", "host of transfer node")
+	delay := flag.Int("delay", 0, "delay before upload is triggered (0 = wait for user input)")
+	help := flag.Bool("h", false, "help")
 	flag.Parse()
+
+	if *help {
+		flag.PrintDefaults()
+		return
+	}
 
 	path, err := getPath()
 	if err != nil {
@@ -57,8 +78,26 @@ func main() {
 
 	conn.ReceivedFileIDCallback = func(fileID string) {
 		fmt.Printf("Download file at: http://%s/download/%s\n", *host, fileID)
-		conn.Conn.Close()
+		waitToUpload(*delay, func() {
+			fmt.Printf("Uploading...")
+			err = conn.UploadFile(path)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Error streaming file: %s\n", err)
+			}
+		})
 	}
+
+	stat, _ := os.Stat(path)
+	lastProgressPercent := uint64(0)
+	conn.ProgressCallback = func(bytes uint64) {
+		progressPercent := float64(bytes) / float64(stat.Size()) * 100
+		progressPercentInt := uint64(progressPercent)
+		if progressPercentInt-lastProgressPercent >= 10 {
+			lastProgressPercent = progressPercentInt
+			fmt.Printf(" ... Progress %s / %s (%.2f%%)\n", humanize.Bytes(bytes), humanize.Bytes(uint64(stat.Size())), progressPercent)
+		}
+	}
+
 	conn.SelectFile(path)
 
 	if conn.Error != nil {
