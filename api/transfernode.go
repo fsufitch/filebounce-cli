e6e5a3ad9e4e7e29c5a40fa4efd032a5bff25b63
Filip Sufitchi <fsufitchi@gmail.com>
2016-11-23 11:43:45 -0500

    Upload file metadata and receive download URL

--- api/transfernode.go
+++ api/transfernode.go
@@ -2,7 +2,10 @@ package api
 
 import (
 	"errors"
+	"mime"
 	"net/http"
+	"os"
+	"path/filepath"
 	"time"
 
 	"github.com/fsufitch/filebounce-cli/protobufs"
@@ -12,24 +15,80 @@ import (
 
 // TransferNodeConnection holds details about a connection to a transfer node
 type TransferNodeConnection struct {
-	Conn    *websocket.Conn
-	AuthKey string
-	FileID  string
-	Error   error
+	Conn                   *websocket.Conn
+	AuthKey                string
+	FileID                 string
+	ReceivedFileIDCallback func(string)
+	Error                  error
 }
 
-func ConnectTransferNode(url string) *TransferNodeConnection {
+func ConnectTransferNode(url string, auth string) (*TransferNodeConnection, chan error) {
 	conn, _, err := websocket.DefaultDialer.Dial(url, http.Header{})
-	return &TransferNodeConnection{Conn: conn, Error: err}
+	newConn := &TransferNodeConnection{Conn: conn, Error: err, AuthKey: auth}
+	doneChan := make(chan error)
+	go newConn.readLoop(doneChan)
+	return newConn, doneChan
 }
 
-func (conn *TransferNodeConnection) readMessage() (buf []byte, err error) {
+func (conn *TransferNodeConnection) readLoop(doneChan chan error) {
+	defer conn.Conn.Close()
+	for {
+		buf, closed, err := conn.readMessage()
+		if err != nil {
+			doneChan <- err
+			break
+		}
+
+		if closed {
+			doneChan <- nil
+			break
+		}
+
+		err = conn.triageMessage(buf)
+		if err != nil {
+			doneChan <- err
+			break
+		}
+	}
+}
+
+func (conn *TransferNodeConnection) readMessage() (buf []byte, closed bool, err error) {
+	closed = false
 	msgType, buf, err := conn.Conn.ReadMessage()
 	if err != nil {
 		return
 	}
-	if msgType != websocket.BinaryMessage {
+	if msgType == websocket.CloseMessage {
+		closed = true
+	} else if msgType != websocket.BinaryMessage {
 		err = errors.New("Unexpected non-binary message type")
+	}
+	return
+}
+
+func (conn *TransferNodeConnection) triageMessage(buf []byte) (err error) {
+	message := &protobufs.TransferNodeToClientMessage{}
+	err = proto.Unmarshal(buf, message)
+	if err != nil {
+		return
+	}
+
+	switch message.GetType() {
+	case protobufs.TransferNodeToClientMessage_ERROR:
+		err = errors.New(message.GetErrorData().String())
+		break
+	case protobufs.TransferNodeToClientMessage_AUTH_SUCCESS:
+		// Nothing to do here
+		break
+	case protobufs.TransferNodeToClientMessage_TRANSFER_CREATED:
+		conn.FileID = message.TransferCreatedData.GetTransferId()
+		if conn.ReceivedFileIDCallback != nil {
+			conn.ReceivedFileIDCallback(conn.FileID)
+		}
+		break
+	default:
+		err = errors.New("Unexpected message type")
+		break
 	}
 	return
 }
@@ -45,32 +104,29 @@ func (conn *TransferNodeConnection) Authenticate() {
 
 	data, _ := proto.Marshal(message)
 	conn.Error = conn.Conn.WriteMessage(websocket.BinaryMessage, data)
-	if conn.Error != nil {
-		return
+}
+
+func (conn *TransferNodeConnection) SelectFile(path string) {
+	fi, _ := os.Stat(path)
+
+	fname := fi.Name()
+	fsize := fi.Size()
+
+	mimetype := mime.TypeByExtension(filepath.Ext(path))
+	if mimetype == "" {
+		mimetype = "application/octet-stream"
 	}
 
-	buf, err := conn.readMessage()
-	if err != nil {
-		conn.Error = err
-		return
+	message := &protobufs.ClientToTransferNodeMessage{
+		Type: protobufs.ClientToTransferNodeMessage_START_UPLOAD,
+		StartData: &protobufs.StartUploadData{
+			Filename: fname,
+			Size:     uint64(fsize),
+			Mimetype: mimetype,
+		},
+		Timestamp: time.Now().Unix(),
 	}
 
-	response := &protobufs.TransferNodeToClientMessage{}
-	err = proto.Unmarshal(buf, response)
-
-	if err != nil {
-		conn.Error = err
-		return
-	}
-
-	switch response.GetType() {
-	case protobufs.TransferNodeToClientMessage_ERROR:
-		conn.Error = errors.New(response.GetErrorData().String())
-		break
-	case protobufs.TransferNodeToClientMessage_AUTH_SUCCESS:
-		break
-	default:
-		conn.Error = errors.New("Unexpected message type")
-		break
-	}
+	data, _ := proto.Marshal(message)
+	conn.Error = conn.Conn.WriteMessage(websocket.BinaryMessage, data)
 }
--- filebounce/cli.go
+++ filebounce/cli.go
@@ -1,14 +1,46 @@
 package main
 
 import (
+	"errors"
+	"flag"
 	"fmt"
+	"os"
 
 	"github.com/fsufitch/filebounce-cli/api"
 )
 
+func getPath() (string, error) {
+	path := flag.Arg(0)
+	if len(path) == 0 {
+		return "", errors.New("No file specified")
+	}
+	fileInfo, err := os.Stat(path)
+	if err != nil {
+		return "", err
+	}
+	if fileInfo.IsDir() {
+		return "", errors.New("Cannot upload a directory")
+	}
+	return path, nil
+}
+
 func main() {
-	conn := api.ConnectTransferNode("ws://localhost:8888/client_ws")
-	conn.AuthKey = "fake auth key"
+	auth := flag.String("auth", "XXX", "authentication key to send to transfer node")
+	host := flag.String("host", "localhost:8888", "host of transfer node")
+	flag.Parse()
+
+	path, err := getPath()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return
+	}
+
+	url := fmt.Sprintf("ws://%s/client_ws", *host)
+	fmt.Printf("Connecting to: %s\n", url)
+	fmt.Printf("Using auth: %s\n", *auth)
+	fmt.Printf("Uploading file: %s\n", path)
+
+	conn, doneChan := api.ConnectTransferNode(url, *auth)
 
 	if conn.Error != nil {
 		fmt.Printf("Error connecting! %s\n", conn.Error)
@@ -18,8 +50,23 @@ func main() {
 	conn.Authenticate()
 
 	if conn.Error != nil {
-		fmt.Printf("Error authenticating! %s\n", conn.Error)
+		fmt.Fprintf(os.Stderr, "Error authenticating! %s\n", conn.Error)
 	} else {
 		fmt.Println("Connection successful!")
+	}
+
+	conn.ReceivedFileIDCallback = func(fileID string) {
+		fmt.Printf("Download file at: http://%s/download/%s\n", *host, fileID)
+		conn.Conn.Close()
+	}
+	conn.SelectFile(path)
+
+	if conn.Error != nil {
+		fmt.Fprintf(os.Stderr, "Error sending file metadata! %s\n", conn.Error)
+	}
+
+	wsErr := <-doneChan
+	if wsErr != nil {
+		fmt.Fprintf(os.Stderr, "Websocket closed with error %s\n", wsErr)
 	}
 }
